f81601c56d850c34e0c1b7eb7514c6d0f1da44a6
bichomundi <89280439+bichomundi@users.noreply.github.com>
2021-09-03 21:13:15 -0300

    Update indicadores.py
    Agrego devolver con percentil

--- indicadores.py
+++ indicadores.py
@@ -1,1 +1,7 @@
-print ("hola mundo")
+def devolver_con_percentil(df):
+  data = df . copy()
+  data["variacion"]  = data["Close"].pct_change() * 100 
+  data.dropna(inplace=True)
+  data["rank_variacion"] = data["variacion"].rank()
+  data["rank_variacion_pct"] = data["variacion"].rank(pct= True)
+  return data
